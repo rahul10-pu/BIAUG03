32c28ebfa5c2916834a37decb52dd857568539e1
Rahul Kumar <rahul10.pu@gmail.com>
2021-09-04 21:01:38 +0530

    datatypes

---
+++ hello.js
@@ -0,0 +1,24 @@
+var d1="Hello JavaScript kjhwveF KJHEVF  LJWef k kjh fekjhwgefj j wfk"
+var name="rahul"
+var NAME="kumar"
+var Name="Sibil"
+var name="Raghav"
+console.log(name) //this is a comment
+console.log(NAME)
+console.log(Name)
+
+var data
+console.log(data)
+//console.log(d1) this line will not execute
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+
+// console.log(true)
+// console.log(false)
+// console.log(12)
+// var bankAccountNumber=871529847
+// console.log(12.453)
+// console.log("true")
+// console.log("false")
+// console.log("12")
