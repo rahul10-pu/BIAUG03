bc6cad721b829fd8c48cc4de41f573283ed872cf
Rahul Kumar <rahul10.pu@gmail.com>
2021-09-12 21:15:49 +0530

    switch and while loop

---
+++ loop.js
@@ -0,0 +1,52 @@
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+
+
+/**
+ 3-Types of loop
+
+ 1. while loop
+SYNTAX:
+    while(condition){
+        Statement
+        increment/decrement
+        Statement
+        Statement
+        Statement
+        Statement
+    }
+ */
+var a=10
+while(0){
+    console.log("Hello 2021")
+    console.log("Hiii")
+    a++
+    console.log(a)
+}
+if(0){
+    console.log("hi")
+}
