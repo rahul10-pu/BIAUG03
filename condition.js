6595db5766886dc21531f9f7558db4b5d4601d87
Rahul Kumar <rahul10.pu@gmail.com>
2021-09-12 19:21:21 +0530

    condition in JS

---
+++ condition.js
@@ -0,0 +1,113 @@
+// console.log(101%2)
+//If I use mod 2 on a number, I can know that the number is even or odd.
+
+// var num=103
+// // console.log("num is even")
+// console.log("num is odd")
+
+
+//syntax , Rule
+/*
+    if(condition){
+        Statment-1
+    }
+
+*/
+if(""){
+    console.log("Heyy, I am under condition")
+    console.log("Heyy, I am under condition")
+    console.log("Heyy, I am under condition")
+    console.log("Heyy, I am under condition")
+    console.log("Heyy, I am under condition")
+}
+//false: undefinedm, null, 0, ""
+// var a=11
+// if(a%2 == 0){
+//     console.log(a+" is even")
+// }
+// if(a%2 != 0){
+//     console.log(a+" is odd")
+// }
+
+
+// if(a%2 == 0){
+//     console.log(a+" is even")
+// } else{
+//     console.log(a+" is odd")
+// }
+
+// //100,90
+// var q=90
+
+// if(q>100){
+//     console.log(q+" is greater than 100")
+// }else if (q<100){
+//     if(q>90){
+//         console.log(q+" is greater than 90")
+//     }else if (q<90){
+        
+//         console.log(q+" is smaller than 90")
+//     }else{
+//         console.log(q+" is equal than 90")
+//     }
+      
+//     console.log(q+" is smaller than 100")
+// }else{
+//     console.log(q+" is equal than 100")
+// }
+  
+
+// var n=10
+// if(n>10){
+//     console.log(1)
+// }else if(n==100){
+//     console.log(2)
+// }else {
+//     console.log(3)
+// }if(n==10){
+//     console.log(4)
+// }
+
+
+// var n=10
+// if(n>10){
+//     console.log(1)
+// }else if(n==100){
+//     if(n==10){
+//         console.log(5)
+//     }else{
+//         console.log(6)
+//     }
+//     console.log(7)
+// }else {
+//     if(n==10){
+//         console.log(8)
+//     }else{
+//         console.log(9)
+//     }
+//     console.log(3)
+// }if(n==10){
+//     console.log(4)
+// }
+
+// var n="10"
+// if(n==10){
+//     console.log(5)
+// }else{
+//     console.log(6)
+// }
+// if(n===10){
+//     console.log(55)
+// }else{
+//     console.log(66)
+// }
+var m="nov"
+if(m=="feb"){
+    console.log(m+" has 28 days")
+}
+
+if(m=="jan" || m=="mar" || m=="may" || m=="jul" || m=="aug" || m=="oct" || m=="dec"  ){
+    console.log(m+" has 31 days")
+}else if(m=="apr" || m=="jun" || m=="sep" || m=="nov" ){
+    console.log(m+" has 30 days")
+}
--- hello.js
+++ hello.js
@@ -1,34 +1,35 @@
-var d1="Hello JavaScript kjhwveF KJHEVF  LJWef k kjh fekjhwgefj j wfk"
-var name="rahul"
-var NAME="kumar"
-var Name="Sibil"
-var name="Raghav"
-console.log(name) //this is a comment
-console.log(NAME)
-console.log(Name)
+// var d1="Hello JavaScript kjhwveF KJHEVF  LJWef k kjh fekjhwgefj j wfk"
+// var name="rahul"
+// var NAME="kumar"
+// var Name="Sibil"
+// var name="Raghav"
+// console.log(name) //this is a comment
+// console.log(NAME)
+// console.log(Name)
 
-var data
-console.log(data)
-//console.log(d1) this line will not execute
-// console.log(d1)
-// console.log(d1)
-// console.log(d1)
+// var data
+// console.log(data)
+// //console.log(d1) this line will not execute
+// // console.log(d1)
+// // console.log(d1)
+// // console.log(d1)
 
-// console.log(true)
-// console.log(false)
-// console.log(12)
-// var bankAccountNumber=871529847
-// console.log(12.453)
-// console.log("true")
-// console.log("false")
-// console.log("12")
-var undefined=10
-console.log(undefined)
-var a=10
-var b=12
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b)
-console.log(a**b)
+// // console.log(true)
+// // console.log(false)
+// // console.log(12)
+// // var bankAccountNumber=871529847
+// // console.log(12.453)
+// // console.log("true")
+// // console.log("false")
+// // console.log("12")
+// var undefined=10
+// console.log(undefined)
+// var a=10
+// var b=12
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+
