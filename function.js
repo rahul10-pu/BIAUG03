e979866fe47a61d64c48e19cb9e4242e9fb7bbce
Rahul Kumar <rahul10.pu@gmail.com>
2021-09-17 21:19:57 +0530

    function

---
+++ function.js
@@ -0,0 +1,60 @@
+// function jarvis(){
+//     console.log("Hey I am jarvis")
+//     for(var a =1 ; a<4 ; a++){
+
+//         console.log(a)
+//     }
+//     console.log("Bye Bye from Jarvis")
+// }
+
+// function eat(a, b){
+//     if(a=="breakfast"){
+//         console.log("Bread")
+//     }  
+//     if(a=="lunch"){
+//         console.log("Idli")
+//     }
+//     if(a=="dinner"){
+//         console.log("Pizza")
+//     }  
+//     console.log(b)
+// }
+// function greet(name){
+//     var msg="Hey "+name+", Welcome to BI"
+//     console.log(msg)
+//     //intead of printing this, I want to return the message to the place
+//     //from where it was called
+// }
+
+// greet("Rahul")
+
+// function square(num){
+//     var result=num*num
+//     return result
+// }
+// function printNumberLessThanTen(number){
+//     for(var a =number ; a<10 ; a++){
+
+//         console.log(a)
+//     }
+    
+// }
+// printNumberLessThanTen(9)
+// printNumberLessThanTen(90)
+// printNumberLessThanTen(-1)
+
+
+// var x=square(10)+20
+// console.log(x)
+
+// var a=10
+// var a="rahul"
+// console.log(a)
+
+var a = 10
+function rand(){
+     a="rahul"
+    console.log(a)
+}
+rand()
+console.log(a)
--- loop.js
+++ loop.js
@@ -40,13 +40,48 @@ SYNTAX:
         Statement
     }
  */
-var a=10
-while(0){
-    console.log("Hello 2021")
-    console.log("Hiii")
-    a++
-    console.log(a)
-}
-if(0){
-    console.log("hi")
-}
+// var a=10
+// while(0){
+//     console.log("Hello 2021")
+//     console.log("Hiii")
+//     a++
+//     console.log(a)
+// }
+// if(0){
+//     console.log("hi")
+// }
+// var a=1
+// do{
+//     a++
+//     console.log(a)
+// }while(a>"4apple")
+
+// console.log(a<"4apple")
+
+
+/**
+ for(initialization ; condition ; icr/decr){
+
+ }
+ */
+
+
+
+// for(var a =1 ; a<4 ; a++){
+
+//     console.log(a)
+// }
+
+
+// var a=1
+// while(a<4){
+//     console.log(a)
+//     a++
+// }
+
+/**
+ function name(){
+
+
+ }
+ */
