36a5372500628e005d184e2537d836d571f7497e
Rahul Kumar <rahul10.pu@gmail.com>
2021-09-18 19:52:27 +0530

    break and continue

--- loop.js
+++ loop.js
@@ -85,3 +85,138 @@ SYNTAX:
 
  }
  */
+ for(var a =1 ; a<4 ; a++){
+    console.log(a+1)
+    continue;
+    console.log(a)
+}
+console.log("-----------------")
+// for(var a =1 ; a<4 ; a++){
+//     if(a==2){
+//         console.log(a)
+//         continue;       
+//    }
+//    console.log(a)
+// }
+
+// for(var a =1 ; a<4 ; a++){
+//     if(a==2){
+//         console.log(a)
+//         a--;
+//         continue;       
+//    }
+//    console.log(a)
+// }
+
+// for(var a =1 ; a<4 ; a++){
+//     if(a==4){
+//         a--
+//         console.log(a)
+//         continue;       
+//    }
+//    console.log(a)
+// }
+
+
+// for(var a =1 ; a<4 ; a++){
+//     if(a==2){
+//         continue;       
+//    }
+//    console.log(a)
+// }
+//Answer : 1 3
+
+// for(var a =1 ; a<4 ; a++){
+//     if(a==2){
+//         break   
+//     }
+//    console.log(a)
+// }
+//Answer: 1
+
+for(var a =1 ; a<4 ; a++){
+    if(a==2){
+        console.log(a)
+        continue;       
+   }
+   break
+   console.log(a)
+}
+//Answer: 
+for(var a =1 ; a<4 ; a++){
+    if(a==2){
+        console.log(a)
+        continue;       
+   }
+   console.log(a)
+   break
+}
+//Answer: 1
+
+for(var a =1 ; a<4 ; a++){
+    if(a==2){
+        console.log(a)
+        break
+        continue;       
+   }
+   console.log(a)
+}
+//Answer: 1,2
+for(var a =1 ; a<4 ; a++){
+    if(a==2){
+        break
+        console.log(a)
+        continue;       
+   }
+   console.log(a)
+}
+//Answer: 1
+console.log("-------------")
+// for(var a =1 ; a<=4 ; a++){
+//   if(a<=1){
+//       continue
+//   }
+//   if(a==4){
+//       break
+//   }
+//   console.log(a)
+// }
+//Answer: 2,3
+
+// for(var a =1 ; a<4 ; a++){
+//     if(a==2){
+//         break    
+//    }
+//     for(var b =1 ; b<4 ; b++){
+//         if(b==2){
+//             break   
+//         }
+//         console.log(b)
+//     }
+//    console.log(a)
+// }
+
+// for(var a =1 ; a<4 ; a++){
+//     if(a==2){
+//         break    
+//    }
+//     for(var b =1 ; b<4 ; b++){
+//         if(b==2){
+//             continue   
+//         }
+//         console.log(b)
+//     }
+//    console.log(a)
+// }
+for(var a =1 ; a<4 ; a++){
+    if(a==2){
+        continue    
+   }
+    for(var b =1 ; b<4 ; b++){
+        if(b==2){
+            break   
+        }
+        console.log(b)
+    }
+   console.log(a)
+}
