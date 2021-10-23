5aa2b056921a8159900e4a3776062de2cd278d76
Rahul Kumar <rahul10.pu@gmail.com>
2021-10-23 20:33:27 +0530

    simple server

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const url=require('url')
+// console.log("I am trying to print http object")
+// console.log(http)
+// const homeApi=function(request, response){
+//     console.log("Heyyyyy I am the homeApi...and I got called by the client")
+//     response.write("Hey I am Home API, and I am the response from the backend")
+//     response.end()
+// }
+
+const server=http.createServer((request, response)=>{
+    if(request.method=='GET'){
+     response.write("Gotcha,, you did a get request")
+     response.end()
+    }
+    if(request.method=='POST'){
+     response.write("Gotcha,, you did a post request")
+     response.end()
+    }
+    if(request.method=='DELETE'){
+     response.write("Gotcha,, you did a delete request")
+     response.end()
+    }
+    if(request.method=='PUT'){
+     response.write("Gotcha,, you did a put request")
+     response.end()
+    }
+    if(request.method=='PATCH'){
+     response.write("Gotcha,, you did a patch request")
+     response.end()
+    }
+
+ }) //http://localhost:8080/
+server.listen(8080)
