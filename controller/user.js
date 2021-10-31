d56896a3406547640490ecf687bf488124b8edff
Rahul Kumar <rahul10.pu@gmail.com>
2021-10-31 11:07:21 +0530

    params querry

--- controller/user.js
+++ controller/user.js
@@ -3,9 +3,12 @@
 
 module.exports={
     getUser:(req,res)=>{
+        console.log(req.query.username)
+        console.log(req.query.password)
         res.send("Hey I am in controller now, get request")
     },
     createUser:(req,res)=>{
+        console.log(req.body)
         res.send("Hey I am in controller now, post request")
     },
     deleteUser:(req,res)=>{
@@ -16,5 +19,15 @@ module.exports={
     },
     patchUser:(req,res)=>{
         res.send("Hey I am in controller now, patch request")
+    },
+    getUserDetailByname:(req,res)=>{
+        console.log(req.params.name)
+        res.send(req.params.name)
     }
 }
+
+// web 3 is coming (dapps) app which is running on some blockchain platform
+// youtube? google 
+// odysee like youtube but it's a dapp
+// web 2 {2004 till now}
+// jobs in dapp or blockchain 
--- router/user.js
+++ router/user.js
@@ -6,17 +6,27 @@ var userRouter=expressClass.Router()
 
 
 //http://localhost:8080/user
-userRouter.get("/",userController.getUser)
+userRouter.get("/",userController.getUser) //http://localhost:8080/user : GET request
+userRouter.get("/:id",userController.getUserDetailByname) //http://localhost:8080/user/jayesh : GET request
 userRouter.post("/",userController.createUser)
 userRouter.delete("/",userController.deleteUser)
-userRouter.put("/",userController.updateUser)
+userRouter.put("/:id",userController.updateUser)
 userRouter.patch("/",userController.patchUser)
 //http://localhost:8080/user/user
 userRouter.get("/user",(req,res)=>{
     res.send("I am get request of user/user api")
 })
-module.exports=userRouter
-
+module.exports=userRouter 
+//passing the data in URL - 2 types - 1.param , 2.Query
+// https://www.google.com/user/jayesh
+// https://www.google.com/user?name=jayesh&&age=10
 
 // / - http://localhost:8080/user + /
 // /user - http://localhost:8080/user + /user
+
+
+// GET - request information ()
+//POST - pass data from frontend to backend via api in JSON format in req
+//delete - 
+//update - pass data from frontend to backend via api in JSON format to update it
+//patch - patch is for minor update
