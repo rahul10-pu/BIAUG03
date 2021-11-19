1163d3fd9da473f42f45ac672336e33fb0e33c38
Rahul Kumar <rahul10.pu@gmail.com>
2021-11-19 21:21:51 +0530

    added signin

--- controller/auth.js
+++ controller/auth.js
@@ -6,6 +6,7 @@
  */
 import db from  '../model/postgres/index.js' //const db = require("../model/index.js")
  const User=db.user 
+ import jwt from 'jsonwebtoken'
 export const signup=(req, res)=>{ //post  in req.body
     const user=new User({
         username:req.body.username,
@@ -21,4 +22,32 @@ export const signup=(req, res)=>{ //post  in req.body
           res.status(500).send(err)
       }
   )
+  }
+  // post : username/password
+  export const signin=(req,res)=>{
+      //1. find the user if exist in the db with the given username
+    User.findOne({where:{username:req.body.username}})
+    .then(
+        (user)=>{
+            if(!user){ 
+                return res.status(404).send({
+                    message:"User Not found"
+                })
+            }
+            //2.
+            if(user.password!=req.body.password){
+                return res.status(401).send({
+                    message:"Password Incorrect"
+                }) 
+            }
+            //3.issue a toekn and give it back to the user
+            var token=jwt.sign({id:user.id},"secret",{
+                expiresIn:86400
+            })
+            res.status(200).send({
+                accessToken:token
+            })
+        }
+    )
+    .catch()
   }
--- router/auth.js
+++ router/auth.js
@@ -1,7 +1,8 @@
-import {signup} from '../controller/auth.js'// const user=require("../controller/user.js")
+import {signup,signin} from '../controller/auth.js'// const user=require("../controller/user.js")
 import { Router } from 'express'
 const router=Router()
 router.post("/signup", signup)
+router.post("/signin", signin)
 // router.post("/signin", signin)
 
 export  default router
